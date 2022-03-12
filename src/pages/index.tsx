3a5c20eb0338ca716287cf3ce4c6efc7c8c66bf3
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-03-12 13:08:08 -0400

    feat: started home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { FiCalendar, FiUser } from 'react-icons/fi'
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -26,7 +28,30 @@ interface HomeProps {
 
 export default function Home() {
   return (
-    <h1>Teste</h1>
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+
+      <main>
+        <div>
+          <img src="/Logo.svg" alt="logo"/>
+
+          <a href="#">
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida</p>
+            <div>
+              <FiCalendar size={20}/>
+              <time>15 Mar 2022</time>
+            </div>
+            <div>
+              <FiUser size={20}/>
+              <time>Joseph Oliveira</time>
+            </div>
+          </a>
+        </div>
+      </main>
+    </>
   )
 }
 
